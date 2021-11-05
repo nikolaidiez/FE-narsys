5a668b0c94dffd397b14561ff7f9bc1d311423aa
nikolaidiez <nikolaidiez@gmail.com>
2021-11-05 12:03:43 +0700

    refresh file

---
+++ precache-manifest.2861ab569e2ea9322223c5fa2cdb6128.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e42f0282da98250bad",
+    "url": "/css/app.e9d794fa.css"
+  },
+  {
+    "revision": "e2d690741329cf1fe526",
+    "url": "/css/chunk-vendors.3c1213e4.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "b73993cd0ec887dd1fa65c7523836cff",
+    "url": "/img/myznarsys_logo.b73993cd.png"
+  },
+  {
+    "revision": "efc5cb68bae2a1077944f1df55ce87cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e42f0282da98250bad",
+    "url": "/js/app.cee6975d.js"
+  },
+  {
+    "revision": "acbbca7babcbb0c6b15c",
+    "url": "/js/chunk-2d216214.8ece6c8e.js"
+  },
+  {
+    "revision": "51ca5c05bede766ec9e8",
+    "url": "/js/chunk-2d216257.57660cd9.js"
+  },
+  {
+    "revision": "2e57577b809b790cea12",
+    "url": "/js/chunk-313d9bce.f2c9cb1b.js"
+  },
+  {
+    "revision": "e2d690741329cf1fe526",
+    "url": "/js/chunk-vendors.184d699f.js"
+  },
+  {
+    "revision": "f175596e22fc97652ba7b8f1943729a2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
